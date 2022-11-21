fb31d8ee8dba9a6d3528128381cd9e72730b188b
DBordallo <117821904+DBordallo@users.noreply.github.com>
2022-11-21 13:54:01 +0100

    tema9

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
+
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
         cliente.edad = 25;
         cliente.nombre = "Pepe";
         cliente.telefono = 915086254;
@@ -11,24 +13,30 @@ public class Main {
         System.out.println("teléfono del cliente:"+cliente.telefono);
         System.out.println("crédito del cliente:"+cliente.credito);
 
+        trabajador.edad = 50;
+        trabajador.nombre = "Julián";
+        trabajador.telefono = 914803254;
+        trabajador.salario = 1578;
+
+        System.out.println("Mi nombre es " + trabajador.nombre + " y tengo " + trabajador.edad + " años. Mi teléfono es el " + trabajador.telefono + " y mi salario es de " + trabajador.salario + " euros.")
+        ;
+
+
+
 
     }
 }
 
-class Persona {
+class Persona{
     int edad;
     String nombre;
     int telefono;
-
-
 }
 
-class Cliente extends Persona {
-    int credito;
-
+class Cliente extends Persona{
+    double credito;
 }
 
-final class Tabajador extends Persona{
-    int salario;
-
+class Trabajador extends Persona{
+    double salario;
 }
